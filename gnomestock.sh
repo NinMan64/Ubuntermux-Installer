f8fa5d0b83c99c14e2b2b9b2d4dcbebb2e2604b5
NinMan64 <kianhopebarlas11@gmail.com>
2025-03-02 12:53:31 +0800

    Add files via upload
    SH Files

---
+++ gnomestock.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt install gnome-shell vanilla-gnome-desktop gnome-terminal gnome-session -y
+sudo apt install yaru-theme-gtk yaru-theme-icon gnome-tweaks dbus-x11 nautilus tigervnc-standalone-server -y
+clear 
+echo "Please enter your vnc password"
+for file in $(find /usr -type f -iname "*login1*"); do 
+    rm -rf $file
+done 
+sleep 4
+vncserver -kill :1
+sleep 2 
+echo "
+#!/bin/bash
+export XDG_SESSION_TYPE=x11
+export XDG_CURRENT_DESKTOP=GNOME
+export GNOME_SHELL_SESSION_MODE=ubuntu
+service dbus start
+dbus-launch gnome-session" >> $HOME/.vnc/xstartup
+echo "vncserver " >> /usr/local/bin/vncstart
+echo "vncserver -kill :* ; rm -rf /tmp/.X1-lock ; rm -rf /tmp/.X11-unix/X1" >> /usr/local/bin/vncstop
+chmod +x $HOME/.vnc/xstartup
+chmod +x /usr/local/bin/vncstart 
+chmod +x /usr/local/bin/vncstop 
+sleep 2
+vncstart 
+sleep 2 
+dbus-launch gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+sleep 2
+dbus-launch gsettings set org.gnome.desktop.interface icon-theme "Yaru-dark"
+sleep 2
+dbus-launch gsettings set org.gnome.desktop.interface cursor-theme "Yaru"
+sleep 2
+dbus-launch gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/warty-final-ubuntu.png'
+sleep 2
+dbus-launch gnome-extensions enable ubuntu-dock@ubuntu.com
+sleep 2
+dbus-launch gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
+sleep 2
+dbus-launch gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+sleep 2
+vncstop 
+
